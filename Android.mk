d7be740ceb96db0b7b6e0a712bee016d6e41eab8
joeDeuce <joelangston1978@gmail.com>
2012-07-02 19:29:16 -0400

    Rename package to com.ota

--- Android.mk
+++ Android.mk
@@ -18,6 +18,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_PACKAGE_NAME := ParanoidOTA
+LOCAL_PACKAGE_NAME := OTA
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PACKAGE)
